5eb1faf7bb9c1e19e0a0b550141905e20622cfdc
Vishal Kachare <vishalkachare323@gmail.com>
2024-12-03 23:40:52 +0530

    Dialog Component with blur backgrond achivement

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,17 +1,19 @@
 
 import './App.css'
+import DialogComponent from './PracticeComponents/DialogComponent'
 import DrawerComponent from './PracticeComponents/DrawerComponent'
 
 function App() {
 
   return (
-    <>
+    <div className='bg-[url(https://png.pngtree.com/background/20230412/original/pngtree-nature-forest-sun-ecology-picture-image_2394782.jpg)] w-full min-h-screen'>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
 
-    <DrawerComponent/>
-    </>
+    {/* <DrawerComponent/> */}
+    <DialogComponent/>
+    </div>
   )
 }
 
---
+++ my-project/src/PracticeComponents/DialogComponent.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+function DialogComponent() {
+    return (
+        <>
+
+        {/* example 1 ------------------------------ */}
+            <Dialog >
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent className=' w-auto p-0 border-none rounded-2xl overflow-hidden '>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className='flex justify-center items-center'>
+                    <img className='hover:scale-125 transition-all duration-700' src="https://i.pinimg.com/236x/af/82/02/af820295ae042cc2f312b07314f7aa55.jpg" alt="" />
+
+                    </div>
+                </DialogContent>
+            </Dialog>
+
+
+
+
+        </>
+    )
+}
+
+export default DialogComponent
